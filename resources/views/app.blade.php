11edf560963e8ae7b24c1c74e0ae148a004fb436
Joe Ferguson <joe@joeferguson.me>
2015-05-28 21:37:15 -0500

    Add edit profile link to layout

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -44,7 +44,8 @@
 						<li class="dropdown">
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
-								<li><a href="{{ url('/auth/logout') }}">Logout</a></li>
+                                <li><a href="{{ route('users.edit', Auth::user()) }}">Edit Profile</a></li>
+                                <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 							</ul>
 						</li>
 					@endif
