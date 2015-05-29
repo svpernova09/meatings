8098609eb537cc847eadebdeb6a4ec73f88a4b67
Joe Ferguson <joe@joeferguson.me>
2015-05-28 22:02:35 -0500

    If the user has input their calendar id, show link to view it

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,6 +45,9 @@
 							<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">{{ Auth::user()->name }} <span class="caret"></span></a>
 							<ul class="dropdown-menu" role="menu">
                                 <li><a href="{{ route('users.edit', Auth::user()) }}">Edit Profile</a></li>
+                                @if (Auth::user()->calendar_id)
+                                    <li><a href="{{ url('/user/' . Auth::user()->id . '/calendar') }}">Show Calendar Events</a></li>
+                                @endif
                                 <li><a href="{{ url('/auth/logout') }}">Logout</a></li>
 							</ul>
 						</li>
